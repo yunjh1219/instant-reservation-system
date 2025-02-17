83d55ccb6e5827de03baaac3fd02f52280c1c0a6
[yunjh1219] <[orenjh1219@gmail.com]>
2025-02-17 14:07:30 +0900

    jwt 토큰 인증방식

--- _jwt_update/src/main/java/edu/du/_waxing_home/controller/MainController.java
+++ _jwt_update/src/main/java/edu/du/_waxing_home/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
+//---
     @GetMapping("/people/waxing")
     public String waxing() {return "pages/people/peopleWaxing";}
 
